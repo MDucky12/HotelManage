cf8526f7949d8a558be1185a9114b9bc1759a80b
LeNhuHoang <lnhoang1234@gmail.com>
2022-11-16 15:44:42 +0700

    test ImageAvatar code

--- DA1/HotelManage/src/com/hotelmanage/main/MyFrame.java
+++ DA1/HotelManage/src/com/hotelmanage/main/MyFrame.java
@@ -5,8 +5,10 @@
 package com.hotelmanage.main;
 
 import com.hotelmanage.ui.button.RoomButton;
+import com.hotelmanage.ui.panel.ImageAvatar;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -25,8 +27,8 @@ public class MyFrame extends JFrame {
         
 
         RoomButton btn1 = new RoomButton();
-        btn1.setSize(70, 70);
-        btn1.setRadius(100);
+        btn1.setSize(100, 70);
+        btn1.setRadius(50);
         btn1.setBorder(null);
 
         btn1.addActionListener(new ActionListener() {
@@ -37,14 +39,19 @@ public class MyFrame extends JFrame {
                 
             }
         });
-
+        
+        ImageAvatar avata = new ImageAvatar();
+        avata.setBounds(200, 10, 100, 100);
+        ImageIcon icon = new ImageIcon("/home/hoang/Desktop/FPOLY/Ky4/DA1/HotelManage/src/com/hotelmanage/image/5918d8e9040516b65f93c75a9c5b8175.jpg");
+        avata.setIcon(icon);
+        
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1000, 500);
         this.setLayout(null);
         this.setVisible(true);
 
         this.add(btn1);
-
+        this.add(avata);
     }
 
 
