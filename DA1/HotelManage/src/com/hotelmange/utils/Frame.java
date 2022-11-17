ad9e296f4723eb73c12dab811fab2c206a1acf0f
Hoang Minh Duc <duchmps23300@fpt.edu.vn>
2022-11-17 16:56:32 +0700

    MainInterface

--- DA1/HotelManage/src/com/hotelmange/utils/Frame.java
+++ DA1/HotelManage/src/com/hotelmange/utils/Frame.java
@@ -10,6 +10,8 @@ import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.BoxLayout;
 //import java.awt.Dimension;
 //import java.awt.Graphics;
 //import java.awt.Graphics2D;
@@ -22,9 +24,8 @@ import javax.swing.JPanel;
  * @author duchmps23300
  */
 public class Frame extends JFrame {
-    
 
-    public Frame() {     
+    public Frame() {
         //Set up JFrame
         this.setLayout(new BorderLayout());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -32,27 +33,27 @@ public class Frame extends JFrame {
         this.setSize(1116, 700);
         this.setUndecorated(true);
         this.setLocationRelativeTo(null);
-        
-        
+
         //
         JPanel pnlWest = new JPanel();
         JPanel pnlEast = new JPanel();
         JPanel pnlIcon = new JPanel();
-        
+
+        pnlIcon.setBounds(10, 10 , 1, 10);
+        pnlIcon.setBackground(Color.red);
+        pnlWest.add(pnlIcon);
+
         pnlEast.setLayout(new CardLayout());
-        pnlWest.setLayout(new CardLayout());
-        
+        pnlWest.setLayout(new BoxLayout(pnlWest, BoxLayout.Y_AXIS));
+
         this.add(pnlEast, BorderLayout.EAST);
         this.add(pnlWest, BorderLayout.WEST);
-        
+
         pnlEast.setPreferredSize(new Dimension(900, 700));
         pnlWest.setPreferredSize(new Dimension(216, 700));
         pnlWest.setBackground(new Color(0x1C1427));
         pnlEast.add(new MainScreen());
-        
-        
 
     }
-       
 
 }
