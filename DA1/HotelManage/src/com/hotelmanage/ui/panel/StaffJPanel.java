35c0b95342a48631c71e3a33790d762f975525a9
Hoang Minh Duc <duchmps23300@fpt.edu.vn>
2022-11-28 10:24:06 +0700

    them class SqlStatement, tinh chinh font chư

--- DA1/HotelManage/src/com/hotelmanage/ui/panel/StaffJPanel.java
+++ DA1/HotelManage/src/com/hotelmanage/ui/panel/StaffJPanel.java
@@ -36,17 +36,26 @@ public class StaffJPanel extends javax.swing.JPanel {
         jLabel5 = new javax.swing.JLabel();
         jScrollPane3 = new javax.swing.JScrollPane();
         jEditorPane1 = new javax.swing.JEditorPane();
+        jLabel6 = new javax.swing.JLabel();
 
         setMinimumSize(new java.awt.Dimension(850, 680));
 
+        imageAvatar1.setBackground(new java.awt.Color(255, 255, 255));
+        imageAvatar1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/hotelmanage/image/5918d8e9040516b65f93c75a9c5b8175.jpg"))); // NOI18N
+
+        jLabel1.setFont(new java.awt.Font("Barlow Condensed", 1, 14)); // NOI18N
         jLabel1.setText("Mã nhân viên:");
 
+        jLabel2.setFont(new java.awt.Font("Barlow Condensed", 1, 14)); // NOI18N
         jLabel2.setText("Họ và tên:");
 
+        jLabel3.setFont(new java.awt.Font("Barlow Condensed", 1, 14)); // NOI18N
         jLabel3.setText("Ngày sinh: ");
 
+        jLabel4.setFont(new java.awt.Font("Barlow Condensed", 1, 14)); // NOI18N
         jLabel4.setText("Chức vụ: ");
 
+        jTable2.setFont(new java.awt.Font("Barlow Condensed", 0, 12)); // NOI18N
         jTable2.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
@@ -60,9 +69,14 @@ public class StaffJPanel extends javax.swing.JPanel {
         ));
         jScrollPane2.setViewportView(jTable2);
 
+        jLabel5.setFont(new java.awt.Font("Barlow Condensed", 1, 14)); // NOI18N
         jLabel5.setText("Ghi chú");
 
         jScrollPane3.setViewportView(jEditorPane1);
+
+        jLabel6.setFont(new java.awt.Font("Barlow Condensed", 1, 24)); // NOI18N
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setText("THÔNG TIN  NHÂN VIÊN");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -83,11 +97,17 @@ public class StaffJPanel extends javax.swing.JPanel {
                             .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 322, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 322, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap(53, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(19, 19, 19))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(68, 68, 68)
+                .addGap(21, 21, 21)
+                .addComponent(jLabel6)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(imageAvatar1, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
@@ -105,7 +125,7 @@ public class StaffJPanel extends javax.swing.JPanel {
                 .addComponent(jLabel5)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(51, Short.MAX_VALUE))
+                .addContainerGap(52, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -118,6 +138,7 @@ public class StaffJPanel extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTable jTable2;
